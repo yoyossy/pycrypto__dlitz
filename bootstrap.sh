4d75b8223040fb134a3c7821d074264639002dd1
Dwayne Litzenberger <dlitz@dlitz.net>
2013-02-02 20:48:04 -0800

    Use autoconf for most compiler options [TODO: python2.1 / python2.2 / -pthread]
    Hopefully this means we'll break on fewer platforms.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,11 @@
 # Generates ./src/config.h.in and ./configure
 
 set -e
-aclocal
-autoheader
-autoconf
+aclocal --force -I m4
+autoconf --force
+autoheader --force
+
+# The following line is needed to generate build-aux/*, but it will fail
+# because we have no Makefile.am
+mkdir -p build-aux
+automake --add-missing --copy || true
